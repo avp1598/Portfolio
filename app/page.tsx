d89e076d738aa69737bcc7dbb53b30c47a6928ef
Aditya Veer Parmar <nakulparmar15@gmail.com>
2023-08-22 10:30:08 +0530

    fix text

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-md text-zinc-500 ">
-          Hi, I am Aditya Veer, creative developer from India, co founder of{" "}
+          Hi, I am Aditya Veer, full stack developer from India, co founder of{" "}
           <Link
             target="_blank"
             href="https://spect.network"
